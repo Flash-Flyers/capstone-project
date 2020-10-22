343726d1a9e97e9aacb6535ce6af05982135b4ef
jamesging97 <jginger5@kent.edu>
2020-10-21 22:36:12 -0400

    Added basic QR code functionality.

--- FlashFlyers/Controllers/EventCreationController.cs
+++ FlashFlyers/Controllers/EventCreationController.cs
@@ -4,11 +4,14 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
+using System.Drawing;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using FlashFlyers.Models;
 using Microsoft.AspNetCore.Hosting;
+using System.Drawing.Imaging;
+using QRCoder;
 
 namespace FlashFlyers.Controllers
 {
@@ -44,13 +47,12 @@ namespace FlashFlyers.Controllers
             if (flyer == null || Path.GetExtension(flyer.FileName) == String.Empty || Path.GetExtension(flyer.FileName) == null)
                 return Content("Flyer not attached, or incorrect file extension.");
 
-            //Need check on file extension.
-
             var rand = new Random();
             int id = 0;
 
             while (_standardDbContext.Find<EventModel>(id) != null || id == 0)
                 id = rand.Next();
+
             var path = String.Concat(Directory.GetCurrentDirectory(), "/wwwroot/", id.ToString(), Path.GetExtension(flyer.FileName));
 
             using (var stream = new FileStream(path, FileMode.Create)) {
@@ -70,10 +72,18 @@ namespace FlashFlyers.Controllers
                 Building = building,
                 Room = room,
                 Campus = campus
-            }); 
+            });
+
+            QRCodeGenerator qrGenerator = new QRCodeGenerator();
+            QRCodeData qrCodeData = qrGenerator.CreateQrCode(id.ToString(),
+            QRCodeGenerator.ECCLevel.Q);
+            QRCode qrCode = new QRCode(qrCodeData);
+            Bitmap qrCodeImage = qrCode.GetGraphic(20);
+            qrCodeImage.Save(Directory.GetCurrentDirectory() + "/wwwroot/" + id.ToString() + "_qr.png", ImageFormat.Png);
 
             _standardDbContext.SaveChanges();
             _standardDbContext.Dispose();
+
             return RedirectToAction("Index");
         }
     }
