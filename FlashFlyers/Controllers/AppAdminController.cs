6d443155cff2f162555b455c84700c6ce4003ba4
jevans63 <jevans63@kent.edu>
2020-10-24 10:03:53 -0400

    setting up app admin view

---
+++ FlashFlyers/Controllers/AppAdminController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using FlashFlyers.Models;
+using Microsoft.AspNetCore.Hosting;
+namespace FlashFlyers.Controllers
+{
+    public class AppAdminController : Controller
+    {
+        private readonly StandardModel _standardDbContext;
+        public AppAdminController(StandardModel standardDbContext)
+        {
+            _standardDbContext = standardDbContext;
+        }
+
+        public IActionResult Index()
+        {
+            return View(_standardDbContext.Events);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
