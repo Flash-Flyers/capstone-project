a0daac2260979dc4bfd08167a2cc28e176fd02e0
jevans63 <jevans63@kent.edu>
2020-11-10 12:13:42 -0500

    adding 48 hour logic to send email before upcoming event

--- FlashFlyers/Controllers/DelaySend.cs
+++ FlashFlyers/Controllers/DelaySend.cs
@@ -13,24 +13,18 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.EntityFrameworkCore;
+using System.Linq;
 
 namespace FlashFlyers.Models
 {
     public class DelaySend : IHostedService
     {
-        //private readonly StandardModel _standardDbContext;
-        //public DelaySend(StandardModel standardDbContext)
-        //{
-        //    _standardDbContext = standardDbContext;
-        //}
-        //private readonly ILogger<DelaySend> _logger;
         private readonly IServiceScopeFactory _serviceScopeFactory;
 
         public DelaySend(IServiceScopeFactory serviceScopeFactory)
         {
             _serviceScopeFactory = serviceScopeFactory;
         }
-
 
         public Task StartAsync(CancellationToken cancellationToken)
         {
@@ -44,34 +38,47 @@ namespace FlashFlyers.Models
             return null;
         }
 
-        //public String[] str = new String[] { "joe@mail.com", "bob@mail.com"};
-        int id = 858821921;
         public void TaskRoutine()
         {
-            using (var scope = _serviceScopeFactory.CreateScope())
+            string twoDays = DateTime.Now.AddHours(48).ToString("yyyy-MM-dd");
+            while (true)
             {
-                var dbContext = scope.ServiceProvider.GetRequiredService<StandardModel>();
+                // getting scope for database context since this is a singleton class
+                using (var scope = _serviceScopeFactory.CreateScope())
+                {
+                    var dbContext = scope.ServiceProvider.GetRequiredService<StandardModel>();
 
-                DateTime nextStop = DateTime.Now.AddSeconds(5);
-                var timeToWait = nextStop - DateTime.Now;
-                var millisToWait = timeToWait.TotalMilliseconds;
-                Thread.Sleep((int)millisToWait);
-                //for (int i = 0; i < str.Length; i++)
-                //{
-                // create email message
-                var email = new MimeMessage();
-                email.From.Add(MailboxAddress.Parse("brendon.cremin75@ethereal.email"));
-                email.To.Add(MailboxAddress.Parse(dbContext.EmailReminder.Find(id).Email));
-                email.Subject = "Test Email Subject";
-                email.Body = new TextPart(TextFormat.Html) { Text = "<h1>Example HTML Message Body</h1>" };
-
-                // send email
-                using var smtp = new MailKit.Net.Smtp.SmtpClient();
-                smtp.Connect("smtp.ethereal.email", 587, SecureSocketOptions.StartTls);
-                smtp.Authenticate("brendon.cremin75@ethereal.email", "BKw63Nr9qJSFEZbwQ7");
-                smtp.Send(email);
-                smtp.Disconnect(true);
-                //}
+                    // looks through database table "EmailReminder" for events with Date occuring 2 days from current date
+                    var upcomingDates = dbContext.EmailReminder.Where(p => p.Date == twoDays);
+                    foreach (var emailRow in upcomingDates)
+                    {
+                        // creates an email message using MimeMessage
+                        // using ethereal email as placeholder email service
+                        // this can be set up with any email service KSU wants to use
+                        var email = new MimeMessage();
+                        email.From.Add(MailboxAddress.Parse("brendon.cremin75@ethereal.email"));
+                        // looks for every email subcribed to an upcoming event(events occuring 2 days from current date)
+                        email.To.Add(MailboxAddress.Parse(emailRow.Email));
+                        email.Subject = DateTime.Now.ToString();
+                        email.Body = new TextPart(TextFormat.Html) 
+                        { 
+                            Text = "This is a reminder for an event you subcribed to. It will occur on " + twoDays 
+                        };
+                        // send the email using smtp
+                        // using ethereal email as placeholder email service
+                        // this can be set up with any email service KSU wants to use
+                        using var smtp = new MailKit.Net.Smtp.SmtpClient();
+                        smtp.Connect("smtp.ethereal.email", 587, SecureSocketOptions.StartTls);
+                        smtp.Authenticate("brendon.cremin75@ethereal.email", "BKw63Nr9qJSFEZbwQ7");
+                        smtp.Send(email);
+                        smtp.Disconnect(true);
+                    }
+                    // checks to send reminders Every 24 Hours
+                    DateTime nextStop = DateTime.Now.AddHours(24);
+                    var timeToWait = nextStop - DateTime.Now;
+                    var millisToWait = timeToWait.TotalMilliseconds;
+                    Thread.Sleep((int)millisToWait);
+                }
             }
         }
     }
--- FlashFlyers/Controllers/EmailController.cs
+++ FlashFlyers/Controllers/EmailController.cs
@@ -25,46 +25,17 @@ namespace FlashFlyers.Controllers
     public class EmailController : Controller
     {
         private readonly StandardModel _standardDbContext;
-        //private readonly DelaySend _send;
-        public EmailController(StandardModel standardDbContext/*, DelaySend send*/)
+        public EmailController(StandardModel standardDbContext)
         {
             _standardDbContext = standardDbContext;
-            //_send = send;
         }
-
-        //public void Test()
-        //{
-        //    _send.TaskRoutine();
-        //    _send.Email();
-        //}
         public IActionResult Index()
         {
             return View();
         }
-        public string getEmail(int i)
-        {
-            return "joe@mail.com";
-        }
 
         public LocalRedirectResult scheduleEmail(int id, string date, string email)
         {
-            //// create email message
-            //var email = new MimeMessage();
-            //email.From.Add(MailboxAddress.Parse("brendon.cremin75@ethereal.email"));
-            //email.To.Add(MailboxAddress.Parse("jevans63@kent.edu"));
-            //email.Subject = "Test Email Subject";
-            //email.Body = new TextPart(TextFormat.Html) { Text = "<h1>Example HTML Message Body</h1>" };
-
-            //// send email
-            //using var smtp = new MailKit.Net.Smtp.SmtpClient();
-            //smtp.Connect("smtp.ethereal.email", 587, SecureSocketOptions.StartTls);
-            //smtp.Authenticate("brendon.cremin75@ethereal.email", "BKw63Nr9qJSFEZbwQ7");
-            //smtp.Send(email);
-            //smtp.Disconnect(true);
-            //string idStr = id.ToString();
-            //string s = "/" + idStr;
-            //return LocalRedirect(s);
-            //Test();
             if (email != null)
             {
                 _standardDbContext.Add(new EmailReminderModel
